b1a3ed62ae061c17513f501a4ac97ab373c30bb1
Manisha Kumari <krimanisha666@gmail.com>
2022-07-18 14:27:03 +0530

    added

---
+++ Array/insert_at_end.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insert_at_end(int arr[], int n, int cap, int x){
+    if(n==cap){
+        cout<<"Array is full"<<endl;
+        return;
+    }
+    arr[n]=x;
+    for(int i=0; i<cap; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n; //size of array
+    cin>>n;
+    int cap; //capacity of array
+    cin>>cap;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    insert_at_end(arr, n, cap, x);
+}
---
+++ Array/insert_at_pos.cpp
@@ -0,0 +1,15 @@
+//Inserting element into array at specific position
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; //size of array
+    cin>>n;
+    int arr[n]; //array of size n
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int x; //element to be inserted
+    cin>>x;
+    
+}
