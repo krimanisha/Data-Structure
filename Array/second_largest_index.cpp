99b040404e64985eb132e91124499bcfaa72f122
Manisha Kumari <krimanisha666@gmail.com>
2022-07-22 09:49:02 +0530

    updated

--- Array/second_largest_index.cpp
+++ Array/second_largest_index.cpp
@@ -25,6 +25,22 @@ int secondLargest(int arr[], int n){
     }
     return res;
 }
+//Efficient Approach:- O(n)
+int secLargest(int arr[], int n){
+    int res= -1, largest=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]>arr[largest]){
+            res=largest;
+            largest=i;
+        }
+        else if(arr[i]!=arr[largest]){
+            if(res==-1 || arr[i]>arr[res]){
+                res=i;
+            }
+        }
+    }
+    return res;
+}
 int main(){
     int n;
     cin>>n;
