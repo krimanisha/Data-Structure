7225f1fac79cb69e0fdeef6d29be0d652b18fde4
Manisha Kumari <krimanisha666@gmail.com>
2022-07-25 10:40:27 +0530

    added

---
+++ Array/reverse_array.cpp
@@ -0,0 +1,26 @@
+//Reverse an array
+//Time Complexity- O(n)
+#include<bits/stdc++.h>
+using namespace std;
+void reverseArray(int arr[], int n){
+    int low=0, high=n-1;
+    while(low<high){
+        int temp=arr[low];
+        arr[low]=arr[high];
+        arr[high]=temp;
+        low++;
+        high--;
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    reverseArray(arr, n);
+}
