4f083e631a2f255e5d27266e79e5d64e8af5efcd
Manisha Kumari <krimanisha666@gmail.com>
2022-07-27 08:00:06 +0530

    added

---
+++ Array/left_rotate_by_one.cpp
@@ -0,0 +1,24 @@
+//Left rotate array by one
+
+#include<bits/stdc++.h>
+using namespace std;
+void leftRotateByone(int arr[], int n){
+    int temp=arr[0];
+    for(int i=0; i<n; i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    leftRotateByone(arr, n);
+}
